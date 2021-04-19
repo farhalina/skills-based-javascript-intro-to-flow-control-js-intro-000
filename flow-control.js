0388bae3beaed1fdeca65f06c1e6c2a8d78cc768
Farha Lina <farlinaha@gmail.com>
2021-04-19 01:33:28 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -27,10 +27,28 @@ function ageChecker(age) {
 
   function switchAge(age) {
   switch(age){
-    case (age >= 13 && age <= 19):
+    case 12
+      console.log("You are a teenager");
+      break;
+    case 14:
+      console.log("You are a teenager");
+      break;
+    case 15:
+      console.log("You are a teenager");
+      break;
+    case 16:
+      console.log("You are a teenager");
+      break;
+    case 17:
+      console.log("You are a teenager");
+      break;
+    case 18:
+      console.log("You are a teenager");
+      break;
+    case 19:
       console.log("You are a teenager");
       break;
     default:
       console.log("You have an age");
   }
-}
+  }
